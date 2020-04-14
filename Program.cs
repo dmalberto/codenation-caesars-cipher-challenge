cc2415b18747d2b6f9536e78c82c8267e160e133
Diego Alberto <dmalberto@outlook.com>
2020-04-14 19:31:28 +0000

    Bug fixed

--- Program.cs
+++ Program.cs
@@ -1,49 +1,42 @@
-﻿using System;
+using System;
 
-namespace codenation
-{
-    class Program
-    {
+namespace codenation {
+    class Program {
         const string token = "";
-        static void Main(string[] args)
-        {
-            Import import = new Import(token);
-            Data data = import.GetData();
-            char[] alphabet = ("abcdefghijklmnopqrstuvwxyz").ToCharArray();
+        static char[] alphabet = ("abcdefghijklmnopqrstuvwxyz").ToCharArray ();
+        static void Main (string[] args) {
+            Import import = new Import (token);
+            Data data = import.GetData ();
+            Console.WriteLine (data);
 
-            char[] codedArray = data.cifrado.ToCharArray();
+            char[] codedArray = data.cifrado.ToCharArray ();
             char[] decodedArray = new char[codedArray.Length];
 
-            Console.WriteLine(data.cifrado);
+            Console.WriteLine (data.cifrado);
 
-            for (int i = 0; i < codedArray.Length; i++)
-            {
-                try
-                {
-                    int oldPos = Array.IndexOf(alphabet, codedArray[i]);
-                    int newPos = GetNewPos(oldPos, data, alphabet.Length);
+            for (int i = 0; i < codedArray.Length; i++) {
+                try {
+                    int oldPos = Array.IndexOf (alphabet, codedArray[i]);
+                    int newPos = GetNewPos (oldPos, data);
 
                     decodedArray[i] = alphabet[newPos];
-                }
-                catch
-                {
+                } catch {
                     decodedArray[i] = codedArray[i];
                 }
             }
 
             foreach (var item in decodedArray)
                 data.decifrado += item;
-            Console.WriteLine(data.decifrado.ToString());
-            data.resumo_criptografico = Hash.coder(data.decifrado);
+            Console.WriteLine (data.decifrado.ToString ());
+            data.resumo_criptografico = Hash.coder (data.decifrado);
 
-            Export.SubmitAnswer(data);
+            Export.SubmitAnswer (data);
         }
-        static int GetNewPos(int oldPos, Data data, int length)
-        {
+        static int GetNewPos (int oldPos, Data data) {
             int pos = oldPos - data.numero_casas;
 
-            if (pos < 0 && Math.Abs(pos) <= data.numero_casas)
-                pos = length - Math.Abs(oldPos) - data.numero_casas;
+            if (pos < 0 && Math.Abs (pos) <= data.numero_casas)
+                pos = alphabet.Length + oldPos - data.numero_casas;
 
             return pos;
         }
